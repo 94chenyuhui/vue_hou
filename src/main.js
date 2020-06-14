0ca887a6a272c77591c0039b6966bfc97d23bb21
chen <a495171765@qq.com>
2020-06-14 23:02:04 +0800

    完成路由跳转进度条效果

--- src/main.js
+++ src/main.js
@@ -12,16 +12,26 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
+// 进度条1- 导入 NProgress 包对应的JS和CSS
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 import axios from 'axios'
 // 配置默认请求路径前缀
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
 // 请求拦截器
 axios.interceptors.request.use(config => {
+  // 进度条2-在 request 拦截器中，展示进度条 NProgress.start()
+  NProgress.start()
   console.log(config)
   // 为请求头对象,添加Token验证的Authorization字段
   // 需要授权的 API ，必须在请求头中使用 `Authorization` 字段提供 `token` 令牌
   config.headers.Authorization = window.sessionStorage.getItem('token')
   // 在最后必须 return config
+  return config
+})
+// 进度条3-在 response 拦截器中，隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
